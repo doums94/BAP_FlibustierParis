5bcea932db8b735012a1f04e0150ef5786c14e6a
DIAKITE Mahamadou <mahamadou.diakite@devinci.fr>
2018-01-10 21:42:34 +0100

    ajout information

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="style.css">
 
     <title>Document</title>
@@ -13,74 +14,96 @@
 
 <body>
 
-<div class="tof">
-    <div class="barre" style="border: 3px solid black">
-        <p>overflow:scroll</p>
+<div class="container">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="tof">
+                <div class="barre" style="border: 3px solid black">
+                    <a class="btn btn-danger" role="button" data-toggle="collapse" href="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                        Link with href
+                    </a>
+                    <div class="collapse" id="collapseExample">
+                        <div class="well">
+                            hcjcjhcjgxcjgxcjgckgkhvkhvjvichkhcchkkchchkchckhchcjgcghgccgjcjgcgjlsjd,bwxnfljd,wxnfvlkdswxnvlkdsjgnv
+                            sdkjgvb;,rndflkgc,nbldfkn,gvld kfxngvlkfdcn;blvgknbkdfdfl,ndfkbnmdfknbmkfnlmlsjmldlmsdmlknbt
+                            dfkjgbnmksfnblkgjnblknbklngbklt;fglkb;rnglkgbemtklf:;jgmlf:;cjgbmeklf:;
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
-<div>
-    <ul class="tabs">
-        <li class="active"><a href="#home">Tailles / modèles</a></li>
-        <li><a href="#mentions">Perles</a></li>
-    </ul>
-    <div class="tabs-content">
-        <div class="tab-content active" id="home">
-            <br>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-4 col-xs-12 col-lg-4 col-sm-12 ">
             <div>
-                <b>Modèle :</b>
-                <br>
-                <br>
-                <input TYPE="radio" NAME=Modele VALUE=Marie>Simple rang
-                <input TYPE="radio" NAME=Modele VALUE=Divorce class="decale1">Double rang
-                <br>
-                <br>
-                <br>
-            </div>
-            <div><b>Tailles des perles :</b></div>
-            <br>
-            <input TYPE="radio" NAME=Perle VALUE=Marie>6 mm
-            <input TYPE="radio" NAME=Perle VALUE=Divorce class="decale2">8 mm
-            <br>
-            <br>
-            <br>
-            <div><b>Tailles du bracelet :</b></div>
-            <br>
-            <input TYPE="radio" NAME=Bracelet VALUE=Marie>S
-            <input TYPE="radio" NAME=Bracelet VALUE=Divorce class="decale3">L
-            <br>
-            <br>
-            <input TYPE="radio" NAME=Bracelet VALUE=Marie >M
-            <input TYPE="radio" NAME=Bracelet VALUE=Divorce class="decale4">XL
-        </div>
-        <div class="tab-content" id="mentions">
-            <div class="doc">
-                <section id="menu1">
-                    <a href="#menu1">Menu 1</a>
-                    <p>usdofuebrdsiukgfzeisdhfoziesdhfoizeuhksdjfv
-                        zesdxfjhzegbsdxfiukjhcznedslxifhknczesdlxj;,fhcnozerkdsjhxfcezkdsjxhfvoeksdjxhfoezkjdx
-                        sjdhfnchzesdkxjfhczelsdixf;khvn osdkux;fj,hvsdkxfujc,herdsxkferjdhxf
-                        erygfuerzygsdfuyzgesdfgizsgdfiuvgrsiudfgivufsiufvhfisuchfiuvhdsiufhvisudhfv
-                        sjdgfviukhsdfvkhfcihvfdkcjhbvkfjchbvkjfhbcgvkjfcgbvjkfsbkvjfncbkvjfckjv,nfbc</p>
-                </section>
+                <ul class="tabs">
+                    <li class="active"><a href="#home">Tailles / modèles</a></li>
+                    <li><a href="#mentions">Perles</a></li>
+                </ul>
+                <div class="tabs-content">
+                    <div class="tab-content active" id="home">
+                        <br>
+                        <div>
+                            <b>Modèle :</b>
+                            <br>
+                            <br>
+                            <input TYPE="radio" NAME=Modele VALUE=Marie>Simple rang
+                            <input TYPE="radio" NAME=Modele VALUE=Divorce class="decale1">Double rang
+                            <br>
+                            <br>
+                            <br>
+                        </div>
+                        <div><b>Tailles des perles :</b></div>
+                        <br>
+                        <input TYPE="radio" NAME=Perle VALUE=Marie>6 mm
+                        <input TYPE="radio" NAME=Perle VALUE=Divorce class="decale2">8 mm
+                        <br>
+                        <br>
+                        <br>
+                        <div><b>Tailles du bracelet :</b></div>
+                        <br>
+                        <input TYPE="radio" NAME=Bracelet VALUE=Marie>S
+                        <input TYPE="radio" NAME=Bracelet VALUE=Divorce class="decale3">L
+                        <br>
+                        <br>
+                        <input TYPE="radio" NAME=Bracelet VALUE=Marie >M
+                        <input TYPE="radio" NAME=Bracelet VALUE=Divorce class="decale4">XL
+                    </div>
+                    <div class="tab-content" id="mentions">
+                        <div class="doc">
+                            <section id="menu1">
+                                <a href="#menu1">Menu 1</a>
+                                <p>usdofuebrdsiukgfzeisdhfoziesdhfoizeuhksdjfv
+                                    zesdxfjhzegbsdxfiukjhcznedslxifhknczesdlxj;oeksdjxhfoezkjdx
+                                    sjdhfnchzesdkxjfhczelsdixf;khvn osdkux;fj,hvsdkxfujc,herdsxkferjdhxf
+                                    erygfuerzygsdfuyzgesdfgizsgdfiuvgrsiudfgivufsiufvhfisuchfiuvhdsiufhvisudhfv
+                                    sjdgfviukhsdfvkhfcihvfdkcjhbvkfjchbvkjfhbcgvkjfcgbvjkfsbkvjfncbkvjfckjv,nfbc</p>
+                            </section>
 
-                <section id="menu2">
-                    <a href="#menu2">Menu 2</a>
-                    <p>usdofuebrdsiukgfzeisdhfoziesdhfoizeuhksdjfv
-                        zesdxfjhzegbsdxfiukjhcznedslxifhknczesdlxj;,fhcnozerkdsjhxfcezkdsjxhfvoeksdjxhfoezkjdx
-                        sjdhfnchzesdkxjfhczelsdixf;khvn osdkux;fj,hvsdkxfujc,herdsxkferjdhxf
-                        erygfuerzygsdfuyzgesdfgizsgdfiuvgrsiudfgivufsiufvhfisuchfiuvhdsiufhvisudhfv
-                        sjdgfviukhsdfvkhfcihvfdkcjhbvkfjchbvkjfhbcgvkjfcgbvjkfsbkvjfncbkvjfckjv,nfbc</p>
-                </section>
+                            <section id="menu2">
+                                <a href="#menu2">Menu 2</a>
+                                <p>usdofuebrdsiukgfzeisdhfoziesdhfoizeuhksdjfv
+                                    zesdxfjhzegbsdxfiukjhcznedslxifhknczesdlxj;,fhcnozerkdsjhxfcezkdsjxhfvoeksdjxhfoezkjdx
+                                    sjdhfnchzesdkxjfhczelsdixf;khvn osdkux;fj,hvsdkxfujc,herdsxkferjdhxf
+                                    erygfuerzygsdfuyzgesdfgizsgdfiuvgrsiudfgivufsiufvhfisuchfiuvhdsiufhvisudhfv
+                                    sjdgfviukhsdfvkhfcihvfdkcjhbvkfjchbvkjfhbcgvkjfcgbvjkfsbkvjfncbkvjfckjv,nfbc</p>
+                            </section>
 
-                <section id="menu3">
-                    <a href="#menu3">Menu 3</a>
-                    <p>usdofuebrdsiukgfzeisdhfoziesdhfoizeuhksdjfv
-                        zesdxfjhzegbsdxfiukjhcznedslxifhknczesdlxj;,fhcnozerkdsjhxfcezkdsjxhfvoeksdjxhfoezkjdx
-                        sjdhfnchzesdkxjfhczelsdixf;khvn osdkux;fj,hvsdkxfujc,herdsxkferjdhxf
-                        erygfuerzygsdfuyzgesdfgizsgdfiuvgrsiudfgivufsiufvhfisuchfiuvhdsiufhvisudhfv
-                        sjdgfviukhsdfvkhfcihvfdkcjhbvkfjchbvkjfhbcgvkjfcgbvjkfsbkvjfncbkvjfckjv,nfbc</p>
-                </section>
+                            <section id="menu3">
+                                <a href="#menu3">Menu 3</a>
+                                <p>usdofuebrdsiukgfzeisdhfoziesdhfoizeuhksdjfv
+                                    zesdxfjhzegbsdxfiukjhcznedslxifhknczesdlxj;,fhcnozerkdsjhxfcezkdsjxhfvoeksdjxhfoezkjdx
+                                    sjdhfnchzesdkxjfhczelsdixf;khvn osdkux;fj,hvsdkxfujc,herdsxkferjdhxf
+                                    erygfuerzygsdfuyzgesdfgizsgdfiuvgrsiudfgivufsiufvhfisuchfiuvhdsiufhvisudhfv
+                                    sjdgfviukhsdfvkhfcihvfdkcjhbvkfjchbvkjfhbcgvkjfcgbvjkfsbkvjfncbkvjfckjv,nfbc</p>
+                            </section>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -90,6 +113,9 @@
 
 
 
+
+<script src="js/bootstrap.js"></script>
+<script src="js/bootstrap.min.js"></script>
 <script src="app.js"></script>
 </body>
 
